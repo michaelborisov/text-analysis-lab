4c095d01ae4bbb5cea8dc89b41a5f92e9f89346c
NengQian <33320365+NengQian@users.noreply.github.com>
2018-09-23 23:50:42 +0200

    remove replicate path

--- src/configuration.py
+++ src/configuration.py
@@ -1,5 +1,4 @@
 FILE_PATH = "LabShare/data/all/json/"
-NON_GERMAN_FILE_PATH = "LabShare/data/non_german_files.txt"
 CLEAN_FILE_PREFIX = "filtered_"
 NON_GERMAN_FILE_PATH = "LabShare/data/non_german_files.txt"
 SHARE_SPACE_FOLDER_PATH = "LabShare/data/group_bitcuda5/"
